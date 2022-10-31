8032568f8785124825449ae79fb669868637feda
lyazj <seeson@pku.edu.cn>
2022-11-01 01:59:20 +0800

    Revise intg.

--- intg/intg-smp.c
+++ intg/intg-smp.c
@@ -17,6 +17,6 @@ int main(void)
   printf("intg_samp-1001: %lg\n", (double)intg_simp(f, 1, 100, 500));
   printf("intg_cheb-11:   %lg\n", (double)intg_cheb(f, 1, 100, 11));
   printf("intg_cheb-101:  %lg\n", (double)intg_cheb(f, 1, 100, 101));
-  printf("intg_cheb-1001:  %lg\n", (double)intg_cheb(f, 1, 100, 1001));
+  printf("intg_cheb-1001: %lg\n", (double)intg_cheb(f, 1, 100, 1001));
   return 0;
 }
